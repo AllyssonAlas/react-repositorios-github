754d402e5eb643da39e658512f449c26ffbeb0a1
Allysson <allyssonmgs@gmail.com>
2020-02-18 14:56:11 -0300

    Estilização da Página Main

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,7 +1,23 @@
 import React from 'react'
+import { FaGithubAlt, FaPlus } from 'react-icons/fa'
 
-import { Title } from './styles'
+import { Container, Form, SubmitButton } from './styles'
 
 export default function Main() {
-  return <Title>Hello World</Title>
+  return (
+    <Container>
+      <h1>
+        <FaGithubAlt />
+        Repositórios
+      </h1>
+
+      <Form onSubmit={() => {}}>
+        <input type="text" placeholder="Adicionar Repositórios" />
+
+        <SubmitButton>
+          <FaPlus color="#FFF" size={14} />
+        </SubmitButton>
+      </Form>
+    </Container>
+  )
 }
