37e64afb52d4aa60021c7d7a94b55b069b3741f6
Allysson <allyssonmgs@gmail.com>
2020-02-19 01:44:18 -0300

    Finalização da página Main e inicio da lógica da página Repository

--- src/pages/Repository/index.js
+++ src/pages/Repository/index.js
@@ -1,5 +1,50 @@
-import React from 'react'
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
 
-export default function Repository() {
-  return <h1>Repository</h1>
+import api from '../../services/api'
+
+export default class Repository extends Component {
+  static propTypes = {
+    match: PropTypes.shape({
+      params: PropTypes.shape({
+        repository: PropTypes.string,
+      }),
+    }).isRequired,
+  }
+
+  state = {
+    repository: {},
+    issues: [],
+    loading: true,
+  }
+
+  componentD
+
+  async componentDidMount() {
+    const { match } = this.props
+
+    const repoName = decodeURIComponent(match.params.repository)
+
+    const [repository, issues] = await Promise.all([
+      api.get(`/repos/${repoName}`),
+      api.get(`/repos/${repoName}/issues`, {
+        params: {
+          stats: 'open',
+          per_page: 5,
+        },
+      }),
+    ])
+
+    this.setState({
+      repository: repository.data,
+      issues: issues.data,
+      loading: false,
+    })
+  }
+
+  render() {
+    const { repository, issue, loading } = this.props
+
+    return <h1>Repository: {}</h1>
+  }
 }
